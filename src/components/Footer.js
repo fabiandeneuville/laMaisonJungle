a9b6353c47ad8de113f309db46a5350cea184356
Deneuville Fabian <deneuville.fabian@live.fr>
2022-08-28 20:34:26 +0200

    Exercise P2C5 done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,19 @@
-import '../styles/Footer.css'
+import '../styles/Footer.css';
+import {useState} from 'react';
 
 function Footer(){
+
+    const [inputValue, setInputValue] = useState('');
+
+    function handleInput(e){
+        setInputValue(e.target.value)
+    }
+
+    function handleBlur(){
+        if(!inputValue.includes('@')){
+            alert('Attention, il n\'y a pas d\'@, ceci n\'est pas une adresse valide.')
+        }
+    }
 
     return (
         <footer className='lmj-footer'>
@@ -8,7 +21,12 @@ function Footer(){
                 Pour les passionné.e.s de plantes 🌿🌱🌵
             </div>
             <div className='lmj-footer-element'>
-
+                <input type="text" 
+                placeholder='Indiquez votre mail'
+                onChange={handleInput}
+                value={inputValue}
+                onBlur={handleBlur}
+                />
             </div>
         </footer>
     )
